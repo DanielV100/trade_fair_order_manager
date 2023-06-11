c01f3a431305fc95f338168db202b486082d711b
Daniel <daniel.vollmer@bechtle.com>
2023-06-11 11:37:12 +0200

    implemented and configured log4j2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    static Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }

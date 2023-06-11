2f9d6305071d52b248838f1bbd3f74a3dca28f4b
Daniel <daniel.vollmer@bechtle.com>
2023-06-11 12:34:18 +0200

    implemented two buttons for start screen

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,6 @@ public class Main {
     static Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         logger.info("Starting application");
-        Start_TradeManager start_tradeManager = new Start_TradeManager();
+        Start_TradeManager start_tradeManager = new Start_TradeManager(400, 300);
     }
 }
--- src/code/view/Start_TradeManager.java
+++ src/code/view/Start_TradeManager.java
@@ -3,6 +3,7 @@ package code.view;
 import code.controller.Controller;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.IOException;
 
 /**
@@ -11,8 +12,28 @@ import java.io.IOException;
 public class Start_TradeManager {
     Controller controller = new Controller();
 
-    public Start_TradeManager() {
+    public Start_TradeManager(int width, int height) {
         JFrame frameStart = new JFrame(controller.getAppProperties("start_trademanager_framestart_name"));
+        frameStart.setSize(width, height);
+        frameStart.setResizable(false);
+        //centering the application on start
+        frameStart.setLocationRelativeTo(null);
+        frameStart.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frameStart.add(createStartView(width, height));
         frameStart.setVisible(true);
+    }
+    private JPanel createStartView(int width, int height) {
+        JPanel jPanelStart = new JPanel();
+        jPanelStart.setLayout(new GridLayout(1, 2));
+        jPanelStart.setSize(width, height);
+        jPanelStart.add(createButton("start_trademanager_buttonneworder"));
+        jPanelStart.add(createButton("start_trademanager_buttonarchive"));
+
+        return jPanelStart;
+    }
+
+    private JButton createButton(String key) {
+        JButton jButton = new JButton(controller.getAppProperties(key));
+        return jButton;
     }
 }
